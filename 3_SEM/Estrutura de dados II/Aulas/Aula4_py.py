5e1fb28ca8acfa92cf36fed2cf58d2c71f6f3863
EGMJ <gabrieledson730@gmail.com>
2023-08-31 21:15:00 -0300

    add aula4 de python

--- 3_SEM/Estrutura de dados II/Aulas/Aula4_py.py
+++ 3_SEM/Estrutura de dados II/Aulas/Aula4_py.py
@@ -8,3 +8,23 @@ name += 1
 print(name)
 
 
+def fatorial(n):
+    if(n == 0):
+        return 1
+    return n*fatorial(n-1)
+
+
+num = int(input("insira o numero"))
+# print(f'O numero fatorial é: {fatorial(num)}')
+# print(f'O numero fatorial '+str(num) + 'é: ',fatorial(num))
+
+
+def fatorialNR(n):
+    x = 1
+    for i in range(2,n+1):
+        x = x * i
+    return x  
+
+# print(f'O numero fatorial é: {fatorialNR(num)}')
+
+def 
