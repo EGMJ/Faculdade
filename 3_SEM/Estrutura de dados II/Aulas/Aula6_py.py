dffa2bae51ffbe9e3349fe0fe0e9273a34fd60a5
EGMJ <gabrieledson730@gmail.com>
2023-10-19 20:17:30 -0300

    add altura na arvore

--- 3_SEM/Estrutura de dados II/Aulas/Aula6_py.py
+++ 3_SEM/Estrutura de dados II/Aulas/Aula6_py.py
@@ -6,10 +6,11 @@
 
 # metodo construtor
 class NodoArvore():
-    def __init__(self, chave = None, esquerda = None, direita = None):
+    def __init__(self, chave = None, esquerda = None, direita = None, altura = None):
         self.chave = chave
         self.esquerda = esquerda
         self.direita = direita
+        self.altura = altura
 
     def __repr__(self):
         return '%s <- %s -> %s' % (self.esquerda and self.esquerda.chave, self.chave, self.direita and self.direita.chave)
@@ -25,6 +26,7 @@ def em_ordem(raiz):
     em_ordem(raiz.direita)
 
 raiz = NodoArvore(3)
+
 '''
 raiz.esquerda = NodoArvore(1)
 raiz.direita = NodoArvore(8)
--- 3_SEM/Estrutura de dados II/Aulas/Aula8_py.py
+++ 3_SEM/Estrutura de dados II/Aulas/Aula8_py.py
@@ -5,6 +5,7 @@ class NodoArvore():
         self.chave = chave
         self.esquerda = esquerda
         self.direita = direita
+        
 
     def __repr__(self):
         return '%s <- %s -> %s' % (self.esquerda and self.esquerda.chave, self.chave, self.direita and self.direita.chave)
